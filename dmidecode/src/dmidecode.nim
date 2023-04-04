d71f31548c17f024454917f13276ca44c22c670f
endlmk <1269467+endlmk@users.noreply.github.com>
2023-04-04 23:36:49 +0900

    Passed return to readkeyvalue. Done.

--- dmidecode/src/dmidecode.nim
+++ dmidecode/src/dmidecode.nim
@@ -18,14 +18,22 @@ type
 type ParseState = enum
   noOp, sectionName, readKeyValue, readList
 
+proc getIdentLevel(line: string): int =
+  for i, c in pairs(line):
+    if not c.isSpaceAscii():
+      return i
+  return 0
+
 proc parseDMI*(source: string): Table[string, Section] =
   var sections = initTable[string, Section]()
   let lines = source.splitLines()
   var s: Section = nil
   var state: ParseState = noOp
-  var key: string
-  for line in lines:
+  var key, value: string
+  for i, line in pairs(lines):
     if line.isEmptyOrWhitespace:
+      if s != nil:
+        sections[s.title] = s
       continue
     if line.startsWith("Handle"):
       s = new Section
@@ -39,17 +47,22 @@ proc parseDMI*(source: string): Table[string, Section] =
       continue
     if state == readKeyValue:
       let kv = line.strip().split(':')
+      key = kv[0].strip()
       if kv.len == 2:
-        key = kv[0].strip()
-        let value = kv[1].strip()
-        s.props[key] = new Property
-        if value.isEmptyOrWhitespace():
-          state = readList
-          continue
-        s.props[key].val = value
+        value = kv[1].strip()
+      else:
+        value = ""
+      s.props[key] = new Property
+      s.props[key].val = value
+      s.props[key].items = newSeq[string]()
+      if i < len(lines) - 1 and getIdentLevel(line) < getIdentLevel(lines[i + 1]):
+        state = readList
+        continue
       continue
     if state == readList:
       s.props[key].items.add(line.strip())
+      if i < len(lines) - 1 and getIdentLevel(line) > getIdentLevel(lines[i + 1]):
+        state = readKeyValue
+        continue
       continue
-  sections[s.title] = s
   return sections
--- dmidecode/tests/dmidecode_test.nim
+++ dmidecode/tests/dmidecode_test.nim
@@ -8,6 +8,8 @@ BIOS Information
     Characteristics:
         PCI is supported
         BIOS is upgradeable
+    BIOS Revision: 1.40
+        Targeted content distribution is supported
 """
 
 var obj: Table[string, dmidecode.Section]
@@ -18,7 +20,9 @@ check obj.hasKey("BIOS Information")
 check obj["BIOS Information"].title == "BIOS Information"
 check obj["BIOS Information"].hadleLine == "Handle 0x0000, DMI type 0, 24 bytes"
 
-check obj["BIOS Information"].props.len == 2
+check obj["BIOS Information"].props.len == 3
 check obj["BIOS Information"].props["Vendor"].val == "LENOVO"
 check obj["BIOS Information"].props["Characteristics"].items == [
     "PCI is supported", "BIOS is upgradeable"]
+check obj["BIOS Information"].props["BIOS Revision"].val == "1.40"
+check obj["BIOS Information"].props["BIOS Revision"].items == ["Targeted content distribution is supported"]
